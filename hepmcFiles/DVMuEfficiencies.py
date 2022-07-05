e667478ba270c2337ac217868adbf1463ccffc6b
Taylor Sussmane <tsussmane1@gmail.com>
2022-07-05 16:48:58 -0400

    added recursive get reconstructables function

--- hepmcFiles/DVMuEfficiencies.py
+++ hepmcFiles/DVMuEfficiencies.py
@@ -32,26 +32,18 @@ def isCharged(particle):
 	if part.charge != 0: return 1
 	else: return 0
 
-def isReconstructable(particle):
-	decay_dist = np.array([particle.end_vertex.position[0] - particle.production_vertex.position[0], particle.end_vertex.position[1] - particle.production_vertex.position[1], particle.end_vertex.position[2] - particle.production_vertex.position[2]])
-	trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
-	if trans_decay_dist > 1:
-		return True
-	else:
-		return False
-
-def get_reconstructable_children(particle):
+#refPos is the end vertex of neutralino
+def get_reconstructable_children(particle, refPos):
+	reconstructables = []
 	for child in particle.children:
-		decay_dist = np.array([child.end_vertex.position[0] - child.production_vertex.position[0], child.end_vertex.position[1] - child.production_vertex.position[1], child.end_vertex.position[2] - child.production_vertex.position[2]])
-		trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
-		if trans_decay_dist > 1:
-			return child
+		decay_dist = np.array([child.end_vertex.position[0] - refPos[0], child.end_vertex.position[1] - refPos[1], child.end_vertex.position[2] - refPos[2]])
+		decayDist_3D = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2 + decay_dist[2]**2)
+		if decayDist_3D > 1:
+			if decayDist_3D > 10000:
+				reconstructables.append(child)
 		else: 
-			for ch in child.children:
-				decay_dist = np.array([ch.end_vertex.position[0] - ch.production_vertex.position[0], ch.end_vertex.position[1] - ch.production_vertex.position[1], ch.end_vertex.position[2] - ch.production_vertex.position[2]])
-				trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
-				if trans_decay_dist > 1: #placeholder value
-					return ch
+			get_reconstructable_children(child, refPos)
+	return reconstructables
 
 outputFile = ROOT.TFile("output.root","RECREATE")
 h = {}
@@ -81,7 +73,7 @@ for i in range(10):
 			properdecaytime.append(iparticle.end_vertex.position[3]/(iparticle.momentum.e/iparticle.momentum.m()))
 			selectedDecayProds = []
 			sumSelDecayProds = ROOT.TLorentzVector()
-			for ipart in get_reconstructable_children(iparticle):
+			for ipart in get_reconstructable_children(iparticle, iparticle.end_vertex.position):
 				if isCharged(ipart) != 0 and ipart.momentum.pt/abs(ipart.charge) > 1000:
 					selectedDecayProds.append(ipart)
 					ipart_momentum = ROOT.TLorentzVector(ipart.momentum.px, ipart.momentum.py, ipart.momentum.pz, ipart.momentum.e)
@@ -97,13 +89,6 @@ for i in range(10):
 
 			if transverseDistance > 4 and transverseDistance < 300 and abs(iparticle.end_vertex.position[2]) < 300 and numSelDecay >= 3 and InvMass > 20000:
 				passedVertexAcc = True 
-			#look at end vertex and find particles coming out of vertex (look into children function and find a way to loop over
-			#make sure children are charged and have pt greater than 1GeV and then add to array of decay products
-			# do the other checks about distance and stuff and then ask if there's more than 3 (selected) decay products from that end vertex
-			# add 4 vec of selected decay products and then find invariant mass of sum and make sure it's larger than 20 GeV
-				#rebuild 4 p that has same 3 p of sum and then assume pion mass instead of original mass for 4th comp
-				#create TLorentz vec of decay prods and then use setM method to set it to pion mass
-				#use this 4vec to find invariant mass
 		if iparticle.pid == 13:
 			rx = iparticle.production_vertex.position[0]
 			ry = iparticle.production_vertex.position[1]
