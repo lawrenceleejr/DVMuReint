000fcfad432abea8d42127bae51f2afdbf008bd7
Taylor Sussmane <tsussmane1@gmail.com>
2022-07-05 11:49:57 -0400

    DVMuEfficiencies.py

--- hepmcFiles/DVMuEfficiencies.py
+++ hepmcFiles/DVMuEfficiencies.py
@@ -6,6 +6,11 @@ ROOT.gROOT.SetBatch()
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from particle import Particle
+
+#--- INFORMATION ON UNITS ---
+#functions give MeV, want to graph in GeV
+#functions give mm
 
 adapter = pyhepmc_ng.ReaderAsciiHepMC2(filename="1TeV_200GeV_1ps.hepmc")
 
@@ -17,15 +22,42 @@ properdecaytime = []
 MET = []
 METacceptance = []
 muonAcc = []
+CHARGED_PION_MASS = 139.570 #MeV
 
 def fit_function(x, A, B):
 	return (A * np.exp(-x/B))
 
+def isCharged(particle):
+	part = Particle.from_pdgid(particle.pid)
+	if part.charge != 0: return 1
+	else: return 0
+
+def isReconstructable(particle):
+	decay_dist = np.array([particle.end_vertex.position[0] - particle.production_vertex.position[0], particle.end_vertex.position[1] - particle.production_vertex.position[1], particle.end_vertex.position[2] - particle.production_vertex.position[2]])
+	trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
+	if trans_decay_dist > 1:
+		return True
+	else:
+		return False
+
+def get_reconstructable_children(particle):
+	for child in particle.children:
+		decay_dist = np.array([child.end_vertex.position[0] - child.production_vertex.position[0], child.end_vertex.position[1] - child.production_vertex.position[1], child.end_vertex.position[2] - child.production_vertex.position[2]])
+		trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
+		if trans_decay_dist > 1:
+			return child
+		else: 
+			for ch in child.children:
+				decay_dist = np.array([ch.end_vertex.position[0] - ch.production_vertex.position[0], ch.end_vertex.position[1] - ch.production_vertex.position[1], ch.end_vertex.position[2] - ch.production_vertex.position[2]])
+				trans_decay_dist = np.sqrt(decay_dist[0]**2 + decay_dist[1]**2)
+				if trans_decay_dist > 1: #placeholder value
+					return ch
+
 outputFile = ROOT.TFile("output.root","RECREATE")
 h = {}
 h = {}
-h["MET"] = ROOT.TH1F("MET", "MET; MET [MeV]; events", 50, 0, 1000)
-h["METacceptance"] = ROOT.TH1F("METacceptance", "MET acceptance; MET [MeV]; events", 50, 0, 1000)
+h["MET"] = ROOT.TH1F("MET", "MET; MET [GeV]; events", 50, 0, 1000)
+h["METacceptance"] = ROOT.TH1F("METacceptance", "MET acceptance; MET [GeV]; events", 50, 0, 1000)
 
 for i in range(10):
 	adapter.read_event(evt)
@@ -36,16 +68,35 @@ for i in range(10):
 	muonEvtAcc = []
 	passedMuEvtAcc = False
 	passedMETacc = False
+	passedVertexAcc = False
 
 	for iparticle in evt.particles:
-		if abs(iparticle.pid) in [12, 13, 14, 16]:
+		if abs(iparticle.pid) in [12, 13, 14, 16]: #including muons in MET
 			p = ROOT.TLorentzVector(iparticle.momentum.px, iparticle.momentum.py, iparticle.momentum.pz, iparticle.momentum.e)
 			sumInvisible += p
 			#add in LLP treatment
-		if iparticle.pid == 1000022: 
+		if iparticle.pid == 1000022 and not iparticle.end_vertex.position: #first neutralino's end vertex is (0,0,0,0)? and that's messing up the following code, so I put the not part? seemed to work, so I assume that means null 
 			#print(dir(iparticle))
 			decaytime.append(iparticle.end_vertex.position[3])
 			properdecaytime.append(iparticle.end_vertex.position[3]/(iparticle.momentum.e/iparticle.momentum.m()))
+			selectedDecayProds = []
+			sumSelDecayProds = ROOT.TLorentzVector()
+			for ipart in get_reconstructable_children(iparticle):
+				if isCharged(ipart) != 0 and ipart.momentum.pt/abs(ipart.charge) > 1000:
+					selectedDecayProds.append(ipart)
+					ipart_momentum = ROOT.TLorentzVector(ipart.momentum.px, ipart.momentum.py, ipart.momentum.pz, ipart.momentum.e)
+
+					ipart_momentum.SetXYZM(ipart_momentum.Px(), ipart_momentum.Py(), ipart_momentum.Pz(),CHARGED_PION_MASS)
+					sumSelDecayProds += ipart_momentum
+
+			endvertex_x = iparticle.end_vertex.position[0]
+			endvertex_y = iparticle.end_vertex.position[1]
+			transverseDistance = sqrt(endvertex_x**2 + endvertex_y**2)
+			numSelDecay = len(selectedDecayProds)
+			InvMass = sumSelDecayProds.M()
+
+			if transverseDistance > 4 and transverseDistance < 300 and abs(iparticle.end_vertex.position[2]) < 300 and numSelDecay >= 3 and InvMass > 20000:
+				passedVertexAcc = True 
 			#look at end vertex and find particles coming out of vertex (look into children function and find a way to loop over
 			#make sure children are charged and have pt greater than 1GeV and then add to array of decay products
 			# do the other checks about distance and stuff and then ask if there's more than 3 (selected) decay products from that end vertex
@@ -54,8 +105,8 @@ for i in range(10):
 				#create TLorentz vec of decay prods and then use setM method to set it to pion mass
 				#use this 4vec to find invariant mass
 		if iparticle.pid == 13:
-			rx = iparticle.end_vertex.position[0]
-			ry = iparticle.end_vertex.position[1]
+			rx = iparticle.production_vertex.position[0]
+			ry = iparticle.production_vertex.position[1]
 			rvec = ROOT.TVector2(rx, ry)
 			r_decay = rvec.Mod()
 			pvec = ROOT.TVector2(iparticle.momentum.px, iparticle.momentum.py)
@@ -64,13 +115,13 @@ for i in range(10):
 				muonEvtAcc.append(iparticle)
 			if iparticle.momentum.pt() > 25 and abs(d0) > 2 and abs(d0) < 300 and iparticle.momentum.abs_eta() < 2.5:
 				muonAcc.append(iparticle)
-
+		
 	if len(muonEvtAcc) > 0:
 		passedMuEvtAcc = True
 
 	MET.append(sumInvisible.Pt())
 
-	if sumInvisible.Pt() > 100:
+	if sumInvisible.Pt() > 100000: 
 		METacceptance.append(sumInvisible.Pt())
 		h["METacceptance"].Fill(sumInvisible.Pt()/1000.)
 	
@@ -91,14 +142,11 @@ outputFile.Write()
 outputFile.Close()
 
 bins = np.linspace(0,.04, 21)
-METbins = np.linspace(0, 1000, 51)
 data_entries, bins = np.histogram(properdecaytime, bins=bins)
-
-MET_entries, METbins = np.histogram(MET, bins=METbins)
 
 binscenters = np.array([0.5 * (bins[i] + bins[i+1]) for i in range(len(bins)-1)])
 popt, pcov = curve_fit(fit_function, xdata=binscenters[2:], ydata=data_entries[2:], p0=[100, .005])
-print(popt)
+#print(popt)
 xspace = np.linspace(0, 0.04, 100000)
 
 fig = plt.figure()
@@ -111,11 +159,3 @@ ax.set_yscale('log')
 plt.xlabel("decay time")
 plt.ylabel("entries")
 plt.savefig("1TeV_200GeV_100ps_decaytime.pdf")
-
- 
-METfig = plt.figure()
-METax = fig.add_subplot(1,1,1)
-METax.bar(binscenters, MET_entries, width=METbins[1]-METbins[0])
-plt.xlabel("MET value")
-plt.ylabel("entries")
-plt.savefig("1TeV_200GeV_100ps_MET.pdf")		
