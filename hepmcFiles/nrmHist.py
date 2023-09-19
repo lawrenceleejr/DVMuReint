e5b9b60b5ca642d2b4d693df67434dcd1c3f2ca5
Taylor Sussmane <tsussmane1@gmail.com>
2023-09-19 11:50:20 -0400

    changed name to sameMassHists

--- hepmcFiles/nrmHist.py
+++ hepmcFiles/nrmHist.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import ROOT
-import numpy as np
-import os
-import sys
 
-mass = str(sys.argv[1])
-SR = str(sys.argv[2])
-nrmHists = ROOT.THStack("Normalized Histograms mass: %s"%mass, "Normalized Histograms mass: %s"%mass)
-
-for ifile in os.listdir("cutflowFiles"):
-	filename = os.fsdecode(ifile)
-	seperatedStrings = filename.split('_')
-        stopMass = int(seperatedStrings[1]) #GeV
-	if stopMass = mass:
-		cutFlowFile = ROOT.TFile("cutflowFiles/%s"%filename, "UPDATE")
-		nrmHist = cutFlowFile.Get("%s normalized cut-flow"%SR)
-		nrmHists.Add(nrmHist)
-
-C = ROOT.TCanvas("C", "", 600, 600)
-nrmHists.Draw()
-C.SaveAs("%sGeV_normHistStack.pdf"%mass)
--- hepmcFiles/sameMassHists.py
+++ hepmcFiles/sameMassHists.py
@@ -9,18 +9,20 @@ mass = str(sys.argv[1])
 SR = str(sys.argv[2])
 
 C = ROOT.TCanvas("C", "C", 800, 600)
-
 i = 0
 
 for ifile in os.listdir("cutflowFiles"):
 	filename = os.fsdecode(ifile)
 	seperatedStrings = filename.split('_')
 	stopMass = seperatedStrings[1] #GeV
+	lifetime = seperatedStrings[3] #ps
 	if stopMass == mass:
 		i += 1
 		cutFlowFile = ROOT.TFile("cutflowFiles/%s"%filename)
-		nrmHist = cutFlowFile.Get("%s normalized cut-flow"%SR)
-		if i == 1: nrmHist.Draw("C")
-		else: nrmHist.Draw("C same")
+		nrmHist = cutFlowFile.Get("%s normalized cut-flow"%SR).Clone("nrmCutFlow%s"%lifetime)
+		nrmHist.Draw("B")
+		if i == 1: nrmHist.Draw("")
+		else: nrmHist.Draw("same")
+
 
 C.SaveAs("%sGeV_normHistStack.pdf"%mass)
